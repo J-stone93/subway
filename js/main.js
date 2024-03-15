5bc9fac15249cdcc10720df9a79a730a39f505a6
jang <treous11@gmail.com>
2024-03-15 16:55:15 +0900

    24.03.15
    -샌드위치 메뉴 탭 설정

--- js/main.js
+++ js/main.js
@@ -6,9 +6,6 @@ new Swiper('.visual .swiper', {
   autoplay: { // 자동 재생 여부
     delay: 5000 // 5초마다 슬라이드 바뀜(기본값: 3000)
   },
-  slidesPerView: 1, // 한 번에 보여줄 슬라이드 개수(기본값: 1)
-  spaceBetween: 10, // 슬라이드 사이 여백(간격) px
-  centeredSlides: true, // 1번 슬라이드가 가운데 보이기
   pagination: { // 페이지네이션 사용
     el: '.visual .swiper-pagination', // 페이지네이션 요소 선택자
     clickable: true // 사용자의 페이지네이션 요소 제어 가능 여부
@@ -17,4 +14,20 @@ new Swiper('.visual .swiper', {
     nextEl: '.visual .swiper-button-next',
     prevEl: '.visual .swiper-button-prev',
   },
+});
+
+// 클랙식 샌드위치
+const clasicEl = document.querySelector('.clasic');
+const menuClasicEl = document.querySelector('.menu-clasic');
+console.log(menuClasicEl);
+clasicEl.addEventListener('click', function() {
+  menuClasicEl.classList.toggle('hide');
+});
+
+// 프레쉬 샌드위치
+const fleshEl = document.querySelector('.flesh');
+const menuFleshEl = document.querySelector('.menu-flesh');
+console.log(menuFleshEl);
+fleshEl.addEventListener('click', function() {
+  menuFleshEl.classList.toggle('hide');
 });
