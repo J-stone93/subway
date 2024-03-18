c1d6a9789a64e3d2a9b534f8422ac40e01b2db02
jang <treous11@gmail.com>
2024-03-18 17:51:35 +0900

    24.03.18
    -메뉴 js foreach

--- js/main.js
+++ js/main.js
@@ -16,18 +16,118 @@ new Swiper('.visual .swiper', {
   },
 });
 
-// 클랙식 샌드위치
-const clasicEl = document.querySelector('.clasic');
-const menuClasicEl = document.querySelector('.menu-clasic');
-console.log(menuClasicEl);
-clasicEl.addEventListener('click', function() {
-  menuClasicEl.classList.toggle('hide');
+// 유튜브 섹션 위에 애니메이션
+gsap.to('.float1', 1.5, {
+  delay: 1, // 얼마나 늦게 애니메이션을 시작할 것인지 지연 시간을 설정
+  y: 15, // 수직으로 얼마나 움직일지 설정, transform: translateY(수치); 와 같음
+  repeat: -1, // 몇 번 반복할지 설정, -1은 무한 반복
+  yoyo: true, // 한 번 재생된 애니메이션을 다시 뒤로 재생
+  ease: Power1.easeInOut // 타이밍 함수 적용, 느리게-빠르게-느리게
+});
+gsap.to('.float2', 2, {
+  delay: 0.5,
+  y: 15,
+  repeat: -1,
+  yoyo: true,
+  ease: Power1.easeInOut
 });
 
+// 클랙식 샌드위치
+// const clasicEl = document.querySelector('.clasic');
+// const menuClasicEl = document.querySelector('.menu-clasic');
+// console.log(menuClasicEl);
+// clasicEl.addEventListener('click', function() {
+//   menuClasicEl.classList.toggle('hide');
+// });
+
 // 프레쉬 샌드위치
-const fleshEl = document.querySelector('.flesh');
-const menuFleshEl = document.querySelector('.menu-flesh');
-console.log(menuFleshEl);
-fleshEl.addEventListener('click', function() {
-  menuFleshEl.classList.toggle('hide');
-});
+// const fleshEl = document.querySelector('.flesh');
+// const menuFleshEl = document.querySelector('.menu-flesh');
+// console.log(menuFleshEl);
+// fleshEl.addEventListener('click', function() {
+//   menuFleshEl.classList.toggle('show');
+// });
+
+// 프리미엄 샌드위치
+
+
+// const clasicEl = document.querySelector('.clasic');
+// const menuClasicEl = document.querySelector('.menu-clasic');
+// const fleshEl = document.querySelector('.flesh');
+// const menuFleshEl = document.querySelector('.menu-flesh');
+// const premiumEl = document.querySelector('.premium');
+// const menuPremiumEl = document.querySelector('.menu-premium');
+// const morningEl = document.querySelector('.morning');
+// const menuMorningEl = document.querySelector('.menu-morning');
+
+
+
+// const clasicEl = document.querySelector('.clasic');
+// const menuClasicEl = document.querySelector('.menu-clasic');
+// clasicEl.addEventListener('click', function(){
+//   if (menuClasicEl.classList.contains('hidden')) {
+//     menuClasicEl.classList.remove('hidden');
+//   } else {
+//     menuMorningEl.classList.add('hidden');
+//     menuPremiumEl.classList.add('hidden');
+//     menuFleshEl.classList.add('hidden');
+//     menuClasicEl.classList.add('hidden');
+//   }
+// });
+// const fleshEl = document.querySelector('.flesh');
+// const menuFleshEl = document.querySelector('.menu-flesh');
+// fleshEl.addEventListener('click', function(){
+//   if (menuFleshEl.classList.contains('hidden')) {
+//     menuFleshEl.classList.remove('hidden');
+//     menuClasicEl.classList.add('hidden');
+//   } else {
+//     menuMorningEl.classList.add('hidden');
+//     menuPremiumEl.classList.add('hidden');
+//     menuFleshEl.classList.add('hidden');
+//   } 
+// });
+// const premiumEl = document.querySelector('.premium');
+// const menuPremiumEl = document.querySelector('.menu-premium');
+// premiumEl.addEventListener('click', function(){
+//   if (menuPremiumEl.classList.contains('hidden')) {
+//     menuPremiumEl.classList.remove('hidden');
+//   } else {
+//     menuPremiumEl.classList.add('hidden');
+//   } 
+// });
+// const morningEl = document.querySelector('.morning');
+// const menuMorningEl = document.querySelector('.menu-morning');
+// morningEl.addEventListener('click', function(){
+//   if (menuMorningEl.classList.contains('hidden')) {
+//     menuMorningEl.classList.remove('hidden');
+//   } else {
+//     menuMorningEl.classList.add('hidden');
+//   } 
+// });
+
+// const clasicEl = document.querySelector('.clasic');
+// const menuClasicEl = document.querySelector('.menu-clasic');
+// clasicEl.addEventListener('click', function(){
+//   menuClasicEl.classList.contains('hidden')
+//   menuClasicEl.classList.remove('hidden')
+//   menuFleshEl.classList.add('hidden')
+//   menuPremiumEl.classList.add('hidden')
+//   menuMorningEl.classList.add('hidden')
+// });
+
+// const fleshEl = document.querySelector('.flesh');
+// const menuFleshEl = document.querySelector('.menu-flesh');
+// clasicEl.addEventListener('click', function(){
+//   menuFleshEl.classList.contains('hidden')
+//   menuFleshEl.classList.remove('hidden')
+//   menuClasicEl.classList.add('hidden')
+//   menuPremiumEl.classList.add('hidden')
+//   menuMorningEl.classList.add('hidden')
+// });
+
+// const premiumEl = document.querySelector('.premium');
+// const menuPremiumEl = document.querySelector('.menu-premium');
+
+// const morningEl = document.querySelector('.morning');
+// const menuMorningEl = document.querySelector('.menu-morning');
+
