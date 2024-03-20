00b9d48be9230652d0f2f791fbd42f9c0bad452c
jang <treous11@gmail.com>
2024-03-20 15:14:29 +0900

    24.03.20
    -반응형
    -로그인페이지

--- js/main.js
+++ js/main.js
@@ -4,16 +4,8 @@ new Swiper('.visual .swiper', {
   direction: 'horizontal', // 수평 슬라이드(기본값)
   loop: true, // 반복 재생 여부, 1 -> 2 -> 3 -> 4 -> 다시 1
   autoplay: { // 자동 재생 여부
-    delay: 5000 // 5초마다 슬라이드 바뀜(기본값: 3000)
-  },
-  pagination: { // 페이지네이션 사용
-    el: '.visual .swiper-pagination', // 페이지네이션 요소 선택자
-    clickable: true // 사용자의 페이지네이션 요소 제어 가능 여부
-  },
-  navigation: { // 슬라이드 이전/다음 버튼 사용
-    nextEl: '.visual .swiper-button-next',
-    prevEl: '.visual .swiper-button-prev',
-  },
+    delay: 3000 // 5초마다 슬라이드 바뀜(기본값: 3000)
+  }
 });
 
 // 유튜브 섹션 위에 애니메이션
@@ -60,8 +52,4 @@ var swiper = new Swiper(".swiper-event", {
   spaceBetween: 100,
   loopAdditionalSlides : 1,
   loop : true,
-  // navigation: {
-  //   nextEl: ".swiper-button-next",
-  //   prevEl: ".swiper-button-prev",
-  // },
 });
